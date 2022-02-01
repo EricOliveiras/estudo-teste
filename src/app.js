1ec52f9005d861c271b698c3b322a45014388397
Eric Diego Oliveira do Nascimento <88443797+EricOliveiras@users.noreply.github.com>
2022-02-01 16:28:55 -0300

    criei a primeira rota

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 require('./database');
+const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 module.exports = app;
